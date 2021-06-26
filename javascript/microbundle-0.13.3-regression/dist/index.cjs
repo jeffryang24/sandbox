712f4f987cc0dc1cd82ce7decb62b90791cb2a4d
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2021-06-26 12:45:12 +0700

    feat(microbundle-0.13.3): add regression reproduction files

---
+++ javascript/microbundle-0.13.3-regression/dist/index.cjs
@@ -0,0 +1,2 @@
+"use strict";function t(t){return t&&"object"==typeof t&&"default"in t?t:{default:t}}var e=t(require("react"));exports.Button=function(t){return e.default.createElement("button",t,"Hello, "+t.name)};
+//# sourceMappingURL=index.cjs.map
---
+++ javascript/microbundle-0.13.3-regression/dist/index.js
@@ -0,0 +1,2 @@
+import e from"react";function t(t){return e.createElement("button",t,"Hello, "+t.name)}export{t as Button};
+//# sourceMappingURL=index.js.map
